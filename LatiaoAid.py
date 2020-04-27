955acf49b18da1e1a05ec064c3007ed47a2b5ecd
Sean Lee <lm19990323@gmail.com>
2020-04-27 11:31:01 +0800

    Reformated the code. Added trivial logger. Enhanced exceptions handling.

--- LatiaoAid.py
+++ LatiaoAid.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from time import sleep
 
 from selenium import webdriver
@@ -9,6 +10,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 GECKODRIVER_PATH = '/usr/local/bin/geckodriver'
 BASE_URL = "https://passport.bilibili.com/login"
 YJZ_CHANNEL = "https://live.bilibili.com/528"
+
+
+def log(s):
+    with open('log.txt', 'a') as f:
+        f.write(f'[{datetime.now().__str__()}] ')
+        f.write(s)
+        f.write('\n')
 
 
 class LatiaoAid:
@@ -34,97 +42,102 @@ class LatiaoAid:
                 WebDriverWait(self.driver, 10).until(
                     ec.presence_of_element_located((By.XPATH, '//div[@class="chat-history-panel"]')))
             except TimeoutError as e:
-                print(e)
+                self.driver.refresh()
                 continue
             break
 
-    def wait_tab_loading(self):
+    def wait_tab_loading(self) -> bool:
         try:
-            element = WebDriverWait(self.driver, 10).until(
+            element = WebDriverWait(self.driver, 5).until(
                 ec.presence_of_element_located((By.XPATH, '//div[starts-with(@class, "function-bar")]'))
             )
         except TimeoutException as e:
-            return False,
+            return False
         else:
-            return True, element
+            return True
+
+    def clear_chat_history_panel(self):
+        while True:
+            try:
+                self.driver.find_element_by_xpath('//span[@class="icon-item icon-font icon-clear"]').click()
+            except ElementClickInterceptedException as e:
+                print("It seems that some thing obscures the clear screen button. Retry in 10 seconds.")
+                sleep(10)
+            else:
+                break
+
+    def wait_and_click(self):
+        """
+        Wait for 辣条s to be collectible and collect on the current tab, until there are not any.
+        :return: No return value.
+        """
+        owner_name = self.driver.find_element_by_xpath('//a[starts-with(@class, "room-owner-username")]').text
+        while True:
+            # Check for remaining 辣条s
+            try:
+                element = self.driver.find_element_by_xpath('//div[starts-with(@class, "function-bar")]')
+            except NoSuchElementException as e:
+                log(f'【{owner_name}】没辣条了')
+                break
+
+            # Wait for 辣条s to be collectible
+            try:
+                while element.text != '点击领奖':
+                    s = element.text.replace('等待开奖', '')
+                    second = int(s.split(":")[1]) + int(s.split(":")[0]) * 60
+                    if second >= 40:
+                        sleep(30)
+                    elif second > 5:
+                        sleep(4)
+                sender_info_text = self.driver.find_element_by_xpath('//div[@class="gift-sender-info"]').text
+                element.click()
+            except NoSuchElementException as e:
+                pass
+            except IndexError as e:
+                pass
+            except ValueError as e:
+                pass
+            except ElementClickInterceptedException as e:
+                # Wait for dynamically generated obstacle to disappear.
+                sleep(2)
+            else:
+                log(f'【{owner_name}】{sender_info_text} {"辣条" if sender_info_text[-1] == "船" else "亲密度"}到手')
+
+    def wait_for_present(self):
+        links = []
+        while True:
+            latiaos = self.driver.find_elements_by_xpath('//div[@class="chat-item  system-msg border-box"]')
+            if len(latiaos) != 0:
+                break
+            sleep(5)
+        for latiao in latiaos:
+            try:
+                links.append(
+                    latiao.find_element_by_tag_name('a').get_attribute('href')
+                )
+            except NoSuchElementException as e:
+                continue
+        self.clear_chat_history_panel()
+        return links
 
     def main(self):
         self.login()
-
         self.base_tab = self.driver.current_window_handle
-
-        while True:
-            links = []
-
-            # Smell for latiao
+        try:
             while True:
-                latiaos = self.driver.find_elements_by_xpath('//div[@class="chat-item  system-msg border-box"]')
-                if len(latiaos) != 0:
-                    break
-                else:
-                    sleep(5)
-            for latiao in latiaos:
-                links.append(
-                    latiao.find_element_by_tag_name('a').get_attribute('href'))  # will throw NoSuchElementException
-
-            # Clear chat history panel
-            while True:
-                try:
-                    self.driver.find_element_by_xpath('//span[@class="icon-item icon-font icon-clear"]').click()
-                except ElementClickInterceptedException as e:
-                    print("It seems that some thing obscures the clear screen button. Retry in 10 seconds.")
-                    sleep(10)
-                else:
-                    break
-
-            # Go to the last lottory
-            # TODO: Utilize all accessible links
-            link = links[-1]
-
-            # Open a new tab
-            self.new_tab(link)
-
-            # Switch driver to new tab
-            windows_after = self.driver.window_handles
-            new_window = [x for x in windows_after if x != self.base_tab][0]
-            self.driver.switch_to.window(new_window)
-
-            # Check whether the necessary parts of a current tab has been loaded
-            check_result = self.wait_tab_loading()
-            if not check_result[0]:
-                self.close_go_back()
-            element = check_result[1]  # IndexError: tuple index out of range
-
-            while True:
-                # Wait for and claim latiao
-                try:
-                    while element.text != '点击领奖':
-                        s = element.text.replace('等待开奖', '')
-                        second = int(s.split(":")[1]) + int(s.split(":")[0]) * 60
-                        if second >= 40:
-                            sleep(30)
-                        elif second > 5:
-                            sleep(4)
-                    element.click()
-                except NoSuchElementException as e:
-                    pass
-                except IndexError as e:
-                    pass
-                except ValueError as e:
-                    pass
-                except ElementClickInterceptedException as e:
-                    # Wait for dynamically generated obstacle to disappear.
-                    sleep(2)
-                else:
-                    print("Latiao obtained")
-
-                # Check if there still are.
-                try:
-                    element = self.driver.find_element_by_xpath('//div[starts-with(@class, "function-bar")]')
-                except NoSuchElementException as e:
-                    print("都没了")
-                    break
-            self.close_go_back()
+                links = self.wait_for_present()
+                for link in links:
+                    self.new_tab(link)
+                    windows_after = self.driver.window_handles
+                    new_window = [x for x in windows_after if x != self.base_tab][0]
+                    self.driver.switch_to.window(new_window)
+                    loaded = self.wait_tab_loading()
+                    if loaded:
+                        self.wait_and_click()
+                    self.close_go_back()
+        finally:
+            self.driver.close()
+            print("Driver closed")
 
 
 if __name__ == '__main__':
