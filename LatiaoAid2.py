cea518b41485617530aaf8c0be22122cdbc90014
Sean Lee <lm19990323@gmail.com>
2020-05-01 10:33:39 +0800

    Under development

--- LatiaoAid2.py
+++ LatiaoAid2.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass, field
-from dataclasses import dataclass, field
 from datetime import datetime, timedelta
 from queue import PriorityQueue
 from time import sleep
 
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, ElementClickInterceptedException, \
+    StaleElementReferenceException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
@@ -33,6 +33,12 @@ class LatiaoAid:
         self.base_tab = None
         self.loot_tab = None
 
+    def delete_element(self, element):
+        self.driver.execute_script("""
+        var element = arguments[0];
+        element.parentNode.removeChild(element);
+        """, element)
+
     def login(self):
         self.driver.get(BASE_URL)
         WebDriverWait(self.driver, 99999).until(ec.url_to_be("https://www.bilibili.com/"))
@@ -48,6 +54,12 @@ class LatiaoAid:
             else:
                 break
         self.base_tab = self.driver.current_window_handle
+        # WebDriverWait(self.driver, 10).until(ec.presence_of_element_located((By.XPATH, '//div[@id="sidebar-vm"]')))
+        # self.delete_element(self.driver.find_element_by_xpath('//div[@id="sidebar-vm"]'))
+        # WebDriverWait(self.driver, 10).until(ec.presence_of_element_located((By.XPATH, '//div[@id="my-dear-haruna-vm"]')))
+        # self.delete_element(self.driver.find_element_by_xpath('//div[@id="my-dear-haruna-vm"]'))
+        # WebDriverWait(self.driver, 10).until(ec.presence_of_element_located((By.XPATH, '//div[@id="live-player-ctnr"]')))
+        # self.delete_element(self.driver.find_element_by_xpath('//div[@id="live-player-ctnr"]'))
 
     def to_loot_tab(self):
         self.driver.switch_to.window(self.loot_tab)
@@ -90,6 +102,7 @@ class LatiaoAid:
                 link = YJZ_CHANNEL
             if link not in self.set:
                 self.waiting_list.append(link)
+                print(link)
                 self.set.add(link)
         self.clear_chat_history_panel()
         return True
@@ -100,7 +113,7 @@ class LatiaoAid:
         tabs = self.driver.window_handles
         self.loot_tab = [tab for tab in tabs if tab != self.base_tab][0]
         self.driver.switch_to.window(self.loot_tab)
-        _ = WebDriverWait(self.driver, 10).until(
+        _ = WebDriverWait(self.driver, 5).until(
             ec.presence_of_element_located((By.XPATH, '//div[starts-with(@class, "function-bar")]'))
         )
 
@@ -127,7 +140,25 @@ class LatiaoAid:
     def collect(self):
         element = self.driver.find_element_by_xpath('//div[starts-with(@class, "function-bar")]')
         sender_info_text = str(self.driver.find_element_by_xpath('//div[@class="gift-sender-info"]').text)
-        element.click()
+        while True:
+            try:
+                element.click()
+            except ElementClickInterceptedException as e:
+                try:
+                    shit = self.driver.find_element_by_xpath('//div[@class="draw-bingo-cntr draw-bingo-cntr"]')
+                    self.delete_element(shit)
+                except NoSuchElementException as ee:
+                    Logger.err('collect', "", e)
+                    Logger.err('collect', "", ee)
+                    Logger.log(f'啥都没领到')
+                    return
+                continue
+            except StaleElementReferenceException as e:
+                Logger.err("collect()", "点击领奖 Staled", e)
+                Logger.log(f'啥都没领到')
+                return
+            else:
+                break
         loot = "辣条" if "赠送" in sender_info_text else \
             "辣条" if "赢得大乱斗PK胜利" in sender_info_text else \
                 "亲密度" if "上任" in sender_info_text else "不知道什么东西"
@@ -142,40 +173,79 @@ class LatiaoAid:
 
     def main(self):
         self.login()
-        while not self.found_broadcast_msg():
-            sleep(5)
-        for link in self.waiting_list:
-            self.load_tab(link)
-            caster_name = self.get_caster_name()
-            waiting_time = self.get_wait_time()
-            self.queue.put(Channel(link, caster_name, datetime.now() + timedelta(seconds=waiting_time)))
-            self.close_tab()
-        while not self.queue.empty():
-            channel = self.queue.get()
-            if self.driver.current_url != channel.link:
-                self.load_tab(channel.link)
-            waiting_time = self.get_wait_time()
-            if waiting_time > 10:
-                if self.driver.current_url != YJZ_CHANNEL:
-                    self.close_tab()
-                for i in range(waiting_time - 10 // 5):
-                    self.found_broadcast_msg()
+        while True:
+            if len(self.waiting_list) == 0:
+                while not self.found_broadcast_msg():
                     sleep(5)
-                if self.driver.current_url != YJZ_CHANNEL:
-                    self.load_tab(channel.link)
-            while self.have_latiao():
+            for link in self.waiting_list:
+                try:
+                    print("Load tab C")
+                    self.load_tab(link)
+                except TimeoutException as e:
+                    print("Timeout")
+                    print("Close tab C")
+                    self.close_tab()
+                    continue
+                caster_name = self.get_caster_name()
                 waiting_time = self.get_wait_time()
-                if waiting_time < 10:
-                    while self.get_wait_time() != 0:
-                        pass
-                    self.collect()
+                self.queue.put(Channel(link, caster_name, datetime.now() + timedelta(seconds=waiting_time)))
+                print("Close tab C")
+                self.close_tab()
+            while not self.queue.empty():
+                channel = self.queue.get()
+                print(channel.caster, channel.link, channel.due_time)
+                Logger.caster = channel.caster
+                if len(self.driver.window_handles) == 2 and self.driver.current_url != channel.link.replace('http',
+                                                                                                            'https'):
+                    try:
+                        self.close_tab()
+                        self.load_tab(channel.link)
+                    except TimeoutException as e:
+                        print(e)
+                        self.set.remove(channel.link)
+                        continue
+                print(self.driver.current_url, channel.link)
+                try:
+                    waiting_time = self.get_wait_time()
+                except NoSuchElementException as e:
+                    print(e)
+                    self.set.remove(channel.link)
+                    continue
+                if waiting_time > 10:
+                    print("too long", waiting_time)
+                    if self.driver.current_url != YJZ_CHANNEL:
+                        self.close_tab()
+                    for i in range((waiting_time - 10) // 5):
+                        self.found_broadcast_msg()
+                        sleep(5)
+                    if self.driver.current_url != YJZ_CHANNEL:
+                        try:
+                            print("Load tab B")
+                            self.load_tab(channel.link)
+                        except TimeoutException as e:
+                            print(e)
+                            self.set.remove(channel.link)
+                            continue
+                while self.have_latiao():
+                    try:
+                        waiting_time = self.get_wait_time()
+                    except NoSuchElementException as e:
+                        print(e)
+                        self.set.remove(channel.link)
+                        continue
+                    if waiting_time < 10:
+                        while self.get_wait_time() != 0:
+                            pass
+                        self.collect()
+                    else:
+                        break
+                if self.have_latiao():
+                    print("Add back", waiting_time)
+                    self.queue.put(
+                        Channel(channel.link, channel.caster, datetime.now() + timedelta(seconds=waiting_time)))
                 else:
-                    break
-            if self.have_latiao():
-                self.queue.put(
-                    Channel(channel.link, channel.caster, datetime.now() + timedelta(seconds=waiting_time)))
-            else:
-                self.set.remove(channel)
+                    print("channel.link = " + channel.link)
+                    self.set.remove(channel.link)
 
 
 if __name__ == '__main__':
