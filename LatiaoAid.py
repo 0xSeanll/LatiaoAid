b93fbc7d8c4a10d839c8db77a0b686bf00eeeed9
Sean Lee <lm19990323@gmail.com>
2020-05-01 10:32:25 +0800

    Performance improvements

--- LatiaoAid.py
+++ LatiaoAid.py
@@ -1,9 +1,10 @@
 import traceback
+from io import BytesIO
 from time import sleep
 
+from PIL import Image
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, TimeoutException, ElementClickInterceptedException, \
-    StaleElementReferenceException, WebDriverException
+from selenium.common.exceptions import *
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
@@ -36,6 +37,12 @@ class LatiaoAid:
     def to_base_tab(self):
         self.driver.switch_to.window(self.base_tab)
 
+    def delete_element(self, element):
+        self.driver.execute_script("""
+        var element = arguments[0];
+        element.parentNode.removeChild(element);
+        """, element)
+
     def load_channel_528(self):
         self.driver.get(YJZ_CHANNEL)
         while True:
@@ -52,6 +59,10 @@ class LatiaoAid:
 
     def login(self):
         self.driver.get(BASE_URL)
+        qrcode_img = self.driver.find_element_by_xpath(
+            '/html/body/div[1]/div/div[2]/div[3]/div[1]/div').screenshot_as_png
+        img = Image.open(BytesIO(qrcode_img))
+        img.show()
         WebDriverWait(self.driver, 99999).until(ec.url_to_be("https://www.bilibili.com/"))
 
     def wait_for_present(self):
@@ -66,8 +77,7 @@ class LatiaoAid:
                 link = latiao.find_element_by_tag_name('a').get_attribute('href')
                 if link not in links:
                     links.append(link)
-            except NoSuchElementException as e:
-                # Logger.err("wait_for_present()", "", e)
+            except NoSuchElementException:
                 if YJZ_CHANNEL not in links:
                     links.append(YJZ_CHANNEL)
                 continue
@@ -80,17 +90,14 @@ class LatiaoAid:
                 self.driver.find_element_by_xpath('//span[@class="icon-item icon-font icon-clear"]').click()
             except ElementClickInterceptedException as _:
                 try:
-                    element = self.driver.find_element_by_xpath('//div[starts-with(@class, "function-bar")]')
+                    shit = self.driver.find_element_by_xpath('//div[@class="link-popup-ctnr"]')
+                    self.delete_element(shit)
                 except NoSuchElementException:
                     Logger.err("clear_chat_history_panel()",
                                "It seems that some thing obscures the clear screen button. Retry in 10 seconds.")
                     sleep(10)
-                else:
-                    try:
-                        element.click()
-                    except ElementClickInterceptedException as e:
-                        Logger.err("clear_chat_history_panel()", "Unable to click 清楚弹幕. Retry.")
-                        continue
+                finally:
+                    continue
             else:
                 break
 
@@ -138,51 +145,23 @@ class LatiaoAid:
             try:
                 element.click()
             except ElementClickInterceptedException as e:
-                # Logger.err("collect()", "Cannot click 点击领奖", e)
-                sleep(1)
+                try:
+                    shit = self.driver.find_element_by_xpath('//div[@class="draw-bingo-cntr draw-bingo-cntr"]')
+                    self.delete_element(shit)
+                except NoSuchElementException as ee:
+                    Logger.err('collect', "", e)
+                    Logger.err('collect', "", ee)
+                    pass
                 continue
             except StaleElementReferenceException as e:
                 Logger.err("collect()", "点击领奖 Staled", e)
                 break
             else:
                 break
-
         loot = "辣条" if "赠送" in sender_info_text else \
             "辣条" if "赢得大乱斗PK胜利" in sender_info_text else \
                 "亲密度" if "上任" in sender_info_text else "不知道什么东西"
-
-        msg = f'{sender_info_text} {loot}到手'
-
-        # ! This message sometimes is printed multiple times
-        Logger.log(msg)
-
-    # def show_loots(self):
-    #     # self.driver.find_element_by_xpath('//div[@class="gift-package live-skin-highlight-bg"]').click()
-    #     # _ = WebDriverWait(self.driver, 10).until(
-    #     #     ec.presence_of_element_located((By.XPATH, '//div[@class="wrap"]'))
-    #     # )
-    #     loots = self.driver.find_elements_by_xpath('//div[@class="gift-item-wrap"]')[1:]
-    #     total = 0
-    #     expired_today = 0
-    #     for loot in loots:
-    #         expiration = loot.find_element_by_class_name("expiration").text
-    #         # try:
-    #         num = int(str(loot.find_element_by_class_name("num").text))
-    #         # except ValueError:
-    #         #     return
-    #         # except StaleElementReferenceException:
-    #         #     return
-    #         total += num
-    #         if expiration == "1天":
-    #             expired_today += num
-    #     Logger.log(f"包里有 {total} 根辣条，今天有 {expired_today} 根到保质期")
-    #     # while True:
-    #     #     try:
-    #     #         self.driver.find_element_by_xpath('//div[@class="gift-package live-skin-highlight-bg"]').click()
-    #     #     except ElementClickInterceptedException:
-    #     #         continue
-    #     #     else:
-    #     #         break
+        Logger.log(f'{sender_info_text} {loot}到手')
 
     def main(self):
         self.login()
@@ -193,10 +172,9 @@ class LatiaoAid:
                     links = self.wait_for_present()
                     for link in links:
                         try:
-                            # TODO Remove duplicate links when finishing processing a link.
                             self.load_new_tab(link)
-                        except TimeoutException as e:
-                            Logger.err("load_new_tab", "load failed", e)
+                        except TimeoutException:
+                            Logger.err("load_new_tab", "Load Failed: " + link)
                             self.close_go_back()
                             continue
 
@@ -207,14 +185,13 @@ class LatiaoAid:
 
                         while True:
                             try:
-                                # TODO: 不应该一直在一个直播间墨迹
                                 self.wait_for_countdown(link)
                                 self.collect()
                             except IndexError as e:
                                 Logger.err("LOOP", "Index Error", e)
                                 traceback.print_exc()
                                 continue
-                            except NoSuchElementException as e:
+                            except NoSuchElementException:
                                 Logger.log("没辣条了")
                                 sleep(1)
                                 self.close_go_back()
@@ -233,15 +210,6 @@ class LatiaoAid:
                                 traceback.print_exc()
                                 self.close_go_back()
                                 break
-
-                        # try:
-                        #     self.driver.find_element_by_xpath(
-                        #         '//div[@class="gift-package live-skin-highlight-bg"]'
-                        #     ).click()
-                        #     self.show_loots()
-                        # except WebDriverException as e:
-                        #     Logger.err("show_loots()", e)
-
             except WebDriverException as e:
                 Logger.err("OUTER LOOP", "Unexpected exception. Recovering.", e)
                 traceback.print_exc()
